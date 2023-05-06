170cfe5dd591a8d70332b44a8b641fc30f642d6a
yanabel1996 <119679434+yanabel1996@users.noreply.github.com>
2023-05-06 19:06:09 +0200

    feat: solve the forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const pathFiles = path.join(__dirname, 'files');
+const pathCopyFiles = path.join(__dirname, 'files-copy');
 
+function copyDir() {
+    fs.mkdir(pathCopyFiles, err => {
+        if (err) throw err;
+    });
+    fs.promises.readdir(pathFiles).then(files => {
+        files.forEach(file => {
+            const nameFile = path.basename(file);
+            const pathFile = path.join(pathFiles, nameFile);
+            const pathCopyFile = path.join(pathCopyFiles, nameFile);
+            fs.copyFile(pathFile, pathCopyFile, err => {
+                if (err) throw err;
+            });
+        });
+    })
+}
+
+copyDir();
