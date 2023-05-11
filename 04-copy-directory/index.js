d9e4d127ab891af2a9ea139507a6993ad6bb862f
yanabel1996 <119679434+yanabel1996@users.noreply.github.com>
2023-05-11 14:09:08 +0200

    fix: fix bugs in forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromise = require('fs/promises');
 
 const pathFiles = path.join(__dirname, 'files');
 const pathCopyFiles = path.join(__dirname, 'files-copy');
 
-function copyDir() {
-    fs.mkdir(pathCopyFiles,
-        {recursive: true,}, 
-         err => {
-        if (err) throw err;
-    });
-    fs.promises.readdir(pathFiles).then(files => {
+async function copyDir() {
+    await fsPromise.rm(pathCopyFiles,
+        {
+            force: true,
+            recursive: true
+        });
+
+    await fsPromise.mkdir(pathCopyFiles,
+        {
+            recursive: true
+        });
+
+    await fsPromise.readdir(pathFiles).then(files => {
         files.forEach(file => {
             const nameFile = path.basename(file);
             const pathFile = path.join(pathFiles, nameFile);
